77f69aefebdb992fd9fb4d6852cbe94ad62314bc
Albert Gao <albertgaohy@gmail.com>
2017-10-01 22:11:40 +1300

    #3 Save the intermediate data

--- utils/parser.py
+++ utils/parser.py
@@ -1,8 +1,8 @@
 import csv
-from utils.mongodb import save_cities, save_countries
 from utils.paths import get_city_data_path, get_country_data_path
 from models.City import City
 from models.Country import Country
+import settings
 
 def read_csv_by_line(file_path, callback):
     with open(file_path) as file_to_parse:
@@ -15,27 +15,48 @@ def read_csv_by_line(file_path, callback):
 #   Parse the cities    #
 #########################
 cities = []
+cities_dict = {}
+
+def save_extra_city_fields(city):
+    def get_attrs_to_add():
+        attrs_to_add = {
+            'geo_name_id': getattr(city, 'geo_name_id')
+        }
+        for attr in settings.CITY_FIELDS_TO_ADD:
+            attrs_to_add[attr] = getattr(city, attr)
+        return attrs_to_add
+
+    if not settings.ADD_CITY_TO_COUNTRY:
+        return
+    
+    if not city.country_code in cities_dict:
+        cities_dict[city.country_code] = []
+    
+    cities_dict[city.country_code].append(get_attrs_to_add())
 
 def parse_city_callback(line):
     city = City(line)
     cities.append(city.to_dict())
-
+    save_extra_city_fields(city)
+    
 def parse_city():
     read_csv_by_line(
         get_city_data_path(), 
         parse_city_callback
     )
-    save_cities(cities)
 
 
 #########################
 #  Parse the countries  #
 #########################
 countires = []
+countries_dict = {}
 
 def parse_country_callback(line):
     country = Country(line)
     countires.append(country.to_dict())
+    if settings.ADD_COUNTRY_TO_CITY:
+        countries_dict[country.iso] = country
 
 
 def parse_country():
@@ -43,4 +64,3 @@ def parse_country():
         get_country_data_path(), 
         parse_country_callback
     )
-    save_countries(countires)
