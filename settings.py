be13651cdb9ddfabc956d618bc3b137ab0e78392
Albert Gao <albertgaohy@gmail.com>
2017-10-01 22:11:08 +1300

    #3 Add settings for duplicate infomation

--- settings.py
+++ settings.py
@@ -6,8 +6,19 @@ CITY_COLLECTION_NAME = 'City'
 COUNTRY_COLLECTION_NAME = 'Country'
 
 # Add the name of field that you don't want to save to the database
-# For instance, You don't want to save the alternate_names from cities collection
+# For instance, You don't want to save the alternate_names to cities collection
 # Just add it: CITY_FIELDS_BLACKLIST = [ 'alternate_names' ]
 # The BLACKLIST is always a list.
 CITY_FIELDS_BLACKLIST = []
 COUNTRY_FIELDS_BLACKLIST = []
+
+# It will duplicate a little data to prevent joint queries
+# So after you retrieve the city info, you don't need to query the according
+# country info using `geo_name_id`, there will be a country property for you. Vice versa
+# And you can customize the fields as well
+# The `geo_name_id` will always be added in case you need to refer each other
+ADD_CITY_TO_COUNTRY = True
+CITY_FIELDS_TO_ADD = ['name']
+
+ADD_COUNTRY_TO_CITY = True
+COUNTRY_FIELDS_TO_ADD = ['country', 'currency_code', 'currency_symbol']
