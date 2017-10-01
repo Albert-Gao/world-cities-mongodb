7e9fdabebdfb155d3aa727d5e5a17bcf7eccd1ee
Albert Gao <albertgaohy@gmail.com>
2017-10-01 22:15:12 +1300

    Close #3, finished

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from utils.mongodb import init
+from utils.mongodb import init, save_cities, save_countries
 from utils.parser import parse_city, parse_country
 
 init()
+
 parse_city()
 parse_country()
+
+save_cities()
+save_countries()
