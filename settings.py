439096c5e401232db9fccc7dccba67b887f2552d
Albert Gao <albertgaohy@gmail.com>
2017-10-09 21:47:34 +1300

    Ignore some default fields to be saved to database

--- settings.py
+++ settings.py
@@ -9,8 +9,27 @@ COUNTRY_COLLECTION_NAME = 'Country'
 # For instance, You don't want to save the alternate_names to cities collection
 # Just add it: CITY_FIELDS_BLACKLIST = [ 'alternate_names' ]
 # The BLACKLIST is always a list.
-CITY_FIELDS_BLACKLIST = []
-COUNTRY_FIELDS_BLACKLIST = []
+CITY_FIELDS_BLACKLIST = [
+    'feature_class',
+    'feature_code',
+    'admin1_code',
+    'admin2_code',
+    'admin3_code',
+    'admin4_code',
+    'population',
+    'elevation',
+    'dem'
+]
+COUNTRY_FIELDS_BLACKLIST = [
+    'ISO_Numeric',
+    'fips',
+    'area',
+    'population',
+    'continent',
+    'tld',
+    'equivalent_fips_code',
+    'neighbours'
+]
 
 # It will duplicate a little data to prevent joint queries
 # So after you retrieve the city info, you don't need to query the according
